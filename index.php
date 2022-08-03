91c904483db64fd96841de32c3cf5ac93c40075b
Cartorio Blasco <tech@cartorioblasco.com.br>
2022-08-03 15:26:40 -0300

    clean project

--- index.php
+++ index.php
@@ -5,68 +5,6 @@ $request = new Request();
 print_r($request->input('idade'));
 die;
 
-$uriRequest = $_SERVER['REQUEST_URI'];
-
-$uri = explode("/",$uriRequest);
-$newUri = array_values(array_filter($uri, function($_URI){
-    return ($_URI != "");
-}));
-
-$rota = isset($newUri[0]) ? $newUri[0] : "";
-
-if ($rota == 'user' && $_SERVER['REQUEST_METHOD'] == 'GET'){
-    if ($rota[1] != '' && $rota[1] != 'create'  && $rota[1] != 'edit') {
-        include('views/user/show.php');
-    } else {
-        include('views/user/index.php');
-    }  
-
-    if ($rota[1] == 'create') {
-        include('views/user/show.php');
-    } 
-     
-}
-
-/*
-
-Classe ser humano
-
-    comunicação
-        falar
-        expressão corporal
-        desenho
-        escrita
-
-    respirar
-
-
-    movimentação
-        engatinhar
-        andar
-        correr
-        nadar
-
-    raciocinio
-
-    tem corpo
-    tem orgãos
-
-
-
-Anderson Trombini14:22
-Uma classe é um conjunto de funções.
-
-Flavio Eduardo Mezei14:24
-A classe é um conjunto de funções que tratam o comportamento de seus objetos através de métodos.
-
-Priscila Polaquini de Macedo Leite14:24
-A classe determina objetos com características semelhantes. 
-ela vai determinar o comportamento dos objetos com os métodos
-*/
-
-
-
-
 ?>
 
 
--- views/auth/salvar.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "Vou salvar o nome: ". $_REQUEST['nome'] . " e o email: " .  $_REQUEST['email'];
